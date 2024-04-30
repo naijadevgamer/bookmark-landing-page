8fbc3747625582ac8c97debd1328f6f995ddabfe
thenaijagamer <sabdullahialaba50@gmail.com>
2024-04-30 21:55:51 +0100

    Feat: Add the smooth navigation feature

---
+++ public/script.js
@@ -0,0 +1,11 @@
+/////////////////////////////////////////////////////
+// PAGE NAVIGATION
+document.querySelector(".nav-link").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
