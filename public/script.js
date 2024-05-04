6114d9d3d0a9e94b9b48c0919cb4c40b05dbe913
thenaijagamer <sabdullahialaba50@gmail.com>
2024-05-04 04:41:40 +0100

    Feat: Add the phone navigation functionality

--- public/script.js
+++ public/script.js
@@ -11,3 +11,14 @@ document.querySelector(".nav-links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Phone navigation
+document.querySelector(".phone-nav ul").addEventListener("click", function (e) {
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains("nav-link")) {
+    document.querySelector("#header-checkbox").checked = false;
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
