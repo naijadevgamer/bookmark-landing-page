0b7798f6a12e0f21136fc54f9e76f7462c883862
thenaijagamer <sabdullahialaba50@gmail.com>
2024-05-27 23:39:46 +0100

    chore: Convert a commonJS import to es module

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 // webpack.config.js
-const path = require("path");
+import { resolve } from "path";
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-  plugins: [],
-  mode: "development",
+export const entry = "./src/index.js";
+export const output = {
+  filename: "main.js",
+  path: resolve(__dirname, "dist"),
 };
+export const module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+      },
+    },
+  ],
+};
+export const plugins = [];
+export const mode = "development";
