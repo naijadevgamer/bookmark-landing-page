59e39502dff9b9ce47684d4687ec3274ae1437f9
thenaijagamer <sabdullahialaba50@gmail.com>
2024-06-06 09:04:07 +0100

    chore: Remove unnecessary packages

--- webpack.config.js
+++ webpack.config.js
@@ -72,7 +72,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "src"),
     },
     compress: true,
     port: 9000,
